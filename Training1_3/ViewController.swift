c13c05bf6d7e7a8f3ef8548eac2cf5e045911d7d
Nikita Kush <kushner.nikita1488@gmail.com>
2020-10-22 18:46:00 +0300

    initial commit

--- Training1_3/ViewController.swift
+++ Training1_3/ViewController.swift
@@ -8,12 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var textDay: UITextField!
+    @IBOutlet weak var textMounth: UITextField!
+    @IBOutlet weak var textYear: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBAction func tapped(_ sender: UIButton) {
+        let calendar = Calendar.current
+        var dateComponents = DateComponents()
+        dateComponents.day = Int(textDay.text!)
+        dateComponents.month = Int(textMounth.text!)
+        dateComponents.year = Int(textYear.text!)
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "EEEE"
+        
+        let date = calendar.date( from: dateComponents)
+        let weekDay = dateFormatter.string(from: date!)
+        resultLabel.text = weekDay
+        
     }
-
-
+   override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+       self.view.endEditing(true)
+   }
+   
 }
 
+//let calendar = Calendar.current
+//var dateComponents = DateComponents()
+//dateComponents.day = Int(textDay.text!)
+//dateComponents.month = Int(textMounth.text!)
+//dateComponents.year = Int(textYear.text!)
+//
+//let dateFormatter = DateFormatter()
+//dateFormatter.dateFormat = "EEEE"
+//
+//let date  = calendar.date(from: dateComponents)
+//let weekDate = dateFormatter.string(from: date!)
+//
+//resultLabel.text = weekDate
